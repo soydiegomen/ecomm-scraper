d2f97adebe75b511cd2eb59421b982291ae0a8e8
Diego Mendoza <dmendoza@inflexionsoftware.com>
2021-08-02 19:46:37 -0500

    Add function to read the url to scrap in a file and save the result as a web page

--- ecomm_scraper/spiders/common_spider.py
+++ ecomm_scraper/spiders/common_spider.py
@@ -1,12 +1,21 @@
 import scrapy
+from scrapy.exceptions import CloseSpider
+from ecomm_scraper.spiders.helpers import SpiderHelper
 
 class ClothingstoreSpider(scrapy.Spider):
-    name = "common_spider"
-    #start_urls = ['https://nataliaa-artwork.company.site/']
-    start_urls = ['http://quotes.toscrape.com/']
+    name = 'common_spider'
+    start_urls = None
+    pages_download = 'pages_download'
+
+    def __init__(self, *args, **kwargs):
+        try:
+            pages_download = self.pages_download
+            self.start_urls = SpiderHelper().get_start_url(pages_download)
+            print('#to_download', self.start_urls)
+        except:
+            raise CloseSpider('get_start_url_exception')
 
     def parse(self, response):
-        filename = f'page-result.html'
+        filename = f'public_html/tienda-uno.html'
         with open(filename, 'wb') as f:
             f.write(response.body)
-        self.log(f'Saved file {filename}')
